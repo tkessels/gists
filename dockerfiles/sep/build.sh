b8f653b8b930e7603f57ccfbdb527cf375362660
tke <tobias.kessels@certbw.de>
2019-05-20 11:04:55 +0200

    fixed tag in build script

--- dockerfiles/sep/build.sh
+++ dockerfiles/sep/build.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 docker build --no-cache -t tabledevil/sep:latest -f sep_dev.dockerfile  .
-tag=$(docker run -it tabledevil/sep:latest version | grep -Pio "(?<=Dockertag: )[\d._]+" )
+tag=$( docker run -it tabledevil/sep:latest tag )
 echo "NEW TAG : ${tag}"
 docker tag tabledevil/sep tabledevil/sep:${tag}
 docker push "tabledevil/sep:${tag}"
