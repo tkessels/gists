85f714d4080e01c77d76c9af0822ddf33aa4fb29
tke <tobias.kessels@certbw.de>
2019-11-19 14:54:03 +0100

    updated docker-proxy script
    uses sudo where necessary
    added off function
    moved docker service reload into function

--- proxy/update_docker_proxy.sh
+++ proxy/update_docker_proxy.sh
@@ -1,6 +1,19 @@
 #!/bin/bash
 
 
+function restart_docker(){
+  sudo systemctl daemon-reload
+  sudo systemctl restart docker
+  #systemctl show docker -p Environment
+}
+
+#remove proxy settings from docker
+if [[ "${1}" == "off" ]]; then
+  echo "TURNING OFF PROXY FOR DOCKER"
+  sudo rm -v /etc/systemd/system/docker.service.d/http-proxy.conf
+  restart_docker
+  exit 0
+fi
 
 #populate proxy_ip and proxy_port variables
 path=$(dirname $(readlink -f "${0}"))
@@ -14,10 +27,9 @@ read -s password
 #encpassword=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "${password}")
 #encpassword=$(echo -n ${password}  | sed -e 's/\@/\\x40/g' -e 's/\!/\\x21/g' -e 's/\$/\\x24/g' -e 's/\*/\\x2a/g' -e 's/\%/\\x25/g' -e 's/\&/\\x26/g' -e 's/\#/\\x30/g')
 encpassword=$(echo -n ${password}  | xxd -p | sed -e 's/\(..\)/%%\1/g' )
-echo "[Service]" > /etc/systemd/system/docker.service.d/http-proxy.conf
-echo 'Environment="HTTP_PROXY=http://'"${username}"':"'"${encpassword}@${proxy_ip}:${proxy_port}/" >> /etc/systemd/system/docker.service.d/http-proxy.conf
-echo 'Environment="HTTPS_PROXY=http://'"${username}"':"'"${encpassword}@${proxy_ip}:${proxy_port}/" >> /etc/systemd/system/docker.service.d/http-proxy.conf
-echo -n '"NO_PROXY=localhost,127.0.0.1"'  >> /etc/systemd/system/docker.service.d/http-proxy.conf
-systemctl daemon-reload
-systemctl restart docker
-#systemctl show docker -p Environment
+echo "[Service]" | sudo tee /etc/systemd/system/docker.service.d/http-proxy.conf >/dev/null
+echo 'Environment="HTTP_PROXY=http://'"${username}"':"'"${encpassword}@${proxy_ip}:${proxy_port}/" | sudo tee -a /etc/systemd/system/docker.service.d/http-proxy.conf >/dev/null
+echo 'Environment="HTTPS_PROXY=http://'"${username}"':"'"${encpassword}@${proxy_ip}:${proxy_port}/" | sudo tee -a /etc/systemd/system/docker.service.d/http-proxy.conf >/dev/null
+echo -n '"NO_PROXY=localhost,127.0.0.1"' | sudo tee -a /etc/systemd/system/docker.service.d/http-proxy.conf >/dev/null
+
+restart_docker
