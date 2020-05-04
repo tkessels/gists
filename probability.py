6d0bb45fe41381dfcb1cbf858436ea702656219d
TKE <tobias.kessels@certbw.de>
2020-05-04 15:08:54 +0200

    Convert probabilty.py to python3

--- probability.py
+++ probability.py
@@ -15,13 +15,13 @@ def main():
         employees.append(0)
 
     count = 0
-    for i in xrange(1, 1000001):
+    for i in range(1, 1000001):
         temp = employees[:]
         shuffle(temp)
         if Counter(temp[0:11])[1] == 4:
             count += 1
 
-    print count / 1000000.
+    print(count / 1000000.)
 
 
 if __name__ == '__main__':
