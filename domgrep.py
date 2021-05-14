f6d1f06917df40446294f85506b16723be84c7a0
TKE <tobias.kessels@certbw.de>
2021-05-14 14:59:58 +0200

    Grep Domainnames from text with domgrep

---
+++ domgrep.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+import re
+import sys
+from urllib.parse import urlparse
+
+pattern=re.compile(r'\d+\.\d+\.\d+\.\d+')
+for line in sys.stdin:
+    line=line.strip()
+    if not line.lower().startswith('http'):
+        line="http://"+line
+    try:
+        p=urlparse(line)
+        if not pattern.search(p.netloc):
+            print(p.netloc)
+    except Exception as e:
+        print(e)
+        pass
